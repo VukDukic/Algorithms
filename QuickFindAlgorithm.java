132839c29e1309111ffa7d6ffb598c7ee0806f35
Vuk Djukich <Vuchina@users.noreply.github.com>
2015-01-31 15:08:23 -0800

    Create QuickFindAlgorithm.java

---
+++ QuickFindAlgorithm.java
@@ -0,0 +1,26 @@
+public class QuickFindUF(int N)
+{
+  private int[];
+  
+  public QuickFindUF(int N)
+  {
+    // set id of each object to itself (N array accesses)
+    id = new int[N];
+    for (int i=0; i<N; i++)
+      id[i] = i;
+  }
+  
+  // check whether p and q are in the same component (1 array accesses)
+  public boolean connected(int p, int q)
+  { return id[p] == id[q]; }
+  
+  // change all entries with id[p] to id[q] (It most 2N + 2 array accesses)
+  public void union(int p, int q)
+  {
+    int pid = id[p];
+    int qid = id [q];
+    for (int i = 0; i < id.length; i++)
+      if (id[i] == pid) id[i] = qid;
+  }
+}
+  
